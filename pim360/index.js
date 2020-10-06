b09fa7c8164128272148f1c8af5f4dddc11f106e
Manobala S <manobala.s@Hotmail.com>
2020-10-06 10:15:28 +0530

    Update index.js

--- pim360/index.js
+++ pim360/index.js
@@ -5,6 +5,8 @@ const json2csv = require("json2csv").parse;
 
 module.exports = async function (context, req) {
 
+    const tag_number = (req.query.tag_number || (req.body && req.body.tag_number));
+
     function authPim() {
         console.log("Authenticating ......")
         let pim = new pimApis(settings);
@@ -26,25 +28,25 @@ module.exports = async function (context, req) {
     let promise = new Promise((resolve, reject) => {
         authPim().then((authResponse) => {
             console.log("Authenticated!")
-            fetchAttributes(tagnumber = "Arsenal", authResponse.access_token)
+            fetchAttributes(tag_number, authResponse.access_token)
                 .then((reposonse) => {
                     console.log("Fetched Attributes!")
-                    let arrkeys = Object.keys(reposonse.attrs);
-                    let obj = {};
-                    let arrModifiedData = [];
-                    arrkeys.map((key) => {
-                        obj[reposonse.attrs[key].name] = reposonse.attrs[key].value;
-                        return '';
-                    });
-                    let fields = Object.keys(obj);
-                    const csv = json2csv(obj, fields);
-                    let fileName = "./generated/" + tagnumber + "-attributes-" + new Date().getTime() + ".csv";
                     try {
-                        // fs.writeFileSync(fileName, csv);
+                        let arrkeys = Object.keys(reposonse.attrs);
+                        let obj = {};
+                        let arrModifiedData = [];
+                        arrkeys.map((key) => {
+                            obj[reposonse.attrs[key].name] = reposonse.attrs[key].value;
+                            return '';
+                        });
+                        let fields = Object.keys(obj);
+                        const csv = json2csv(obj, fields);
                         resolve(csv)
                     } catch (error) {
-                        resolve(err.message)
+                        resolve("Something Went Wrong")
                     }
+                }).catch(() => {
+                    resolve("Not Found / Something Went Wrong")
                 })
         });
 
@@ -55,7 +57,7 @@ module.exports = async function (context, req) {
         // status: 200, /* Defaults to 200 */
         headers: {
             'Content-Type': 'text/csv',
-            "Content-Disposition": `attachment; filename=attributes.csv`
+            "Content-Disposition": `attachment; filename=${tag_number + "-attributes-" + new Date().getTime() + ".csv"}`
         },
         body: result
     };
