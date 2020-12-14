a484c8ffff0d70c4ff3a4cead1b9d4686c50e781
Manobala S <manobala.s@Hotmail.com>
2020-12-14 21:16:50 +0530

    Update importfun.js

--- pim360/functions/importfun.js
+++ pim360/functions/importfun.js
@@ -60,25 +60,15 @@ const allUsers = [
 const upload = function (file, some) {
     return new Promise((resolve, reject) => {
         try {
-            // var json = {
-            //     foo: 'bar',
-            //     qux: 'moo',
-            //     poo: 123,
-            //     stux: new Date()
-            // }
-            // var xlsx = json2xlsx(json);
-            // fs.writeFileSync('D:/local/Temp/data.json', xlsx);
             const filename = 'D:/local/Temp/sample.xlsx';
             var xls = json2xls(allUsers);
             fs.writeFileSync(filename, xls, 'binary', (err) => {
                 if (err) {
-                    // console.log("writeFileSync :", err);
                     resolve({ "writeFileSync": err })
                 }
                 resolve({ "writeFileSync": "file is saved" })
             });
             resolve({ "writeFileSync": "file is saved" })
-            // resolve({ "response": "test!" })
         } catch (err) {
             resolve({ "response": JSON.stringify(err) })
         }
@@ -94,8 +84,6 @@ const download = function (file, some) {
         }
     });
 }
-
-// fs.readFileSync('D:/local/Temp/settings.json')
 
 exports.upload = upload;
 exports.download = download;
